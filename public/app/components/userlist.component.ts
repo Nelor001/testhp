9217bb65e7a82beb7b8d6b865c01c5e030419b1c
Nelor <sionjaro@5fold.de>
2016-05-26 15:20:09 +0200

    1.: Cleanup work in userlist component to get rid of most of the ChangeDetectionStrategy related stuff. Left one line as a reminder to optimize this down the road. 2.: userlist.component.ts no uses ngOnInit().

--- public/app/components/userlist.component.ts
+++ public/app/components/userlist.component.ts
@@ -1,18 +1,15 @@
-import { Component, provide, NgZone,ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
-/*import 'zone.js';
-import 'reflect-metadata';
-*/
+import { Component, provide, NgZone} from '@angular/core';
 
 import { userService } from '../services/user.service.ts';
 import { socketService } from '../services/socket.service.ts';
 
 @Component({
-selector:'hp-userlist',
+	selector:'hp-userlist',
 	providers: [ provide(userService, { useClass: userService})
 	,provide(socketService, { useClass: socketService})
 	],
-	templateUrl: 'app/templates/userlist.template.html'//,
-	//changeDetection: ChangeDetectionStrategy.OnPush
+	templateUrl: 'app/templates/userlist.template.html'
+	//,changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserlistComponent
 {
@@ -29,27 +26,17 @@ export class UserlistComponent
 		this.zone = new NgZone({enableLargeStacktrace: true});
 		this.userService = _userService;
 		this.socket  = _socketService.getIO();
-		/*setInterval(() =>
-		{
-			this._cd.markForCheck();
-		},500);*/
-	/*}
+	}
 	
 	ngOnInit()
-	{*/
+	{
 		this.socket.on('users created', (user) =>
 		{
 			
 			this.zone.run(() =>
 			{
-				//this.users.push({"login":user.login});
-				this.users = [...this.users, {"login":user.login}];
+				this.users.push({"login":user.login});
 				console.log('Event: User <'+user.login+'> created.');
-				console.log(user);
-				console.log(this);
-				console.log(this.users);
-				console.log(this.users[3].login);
-				//this._cd.markForCheck();
 			});
 		});
 		console.log('testhp: Userlist registered users created.')
