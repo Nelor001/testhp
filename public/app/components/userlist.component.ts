70586914c92f4bd8166ba7e145c29afda2c9a8b0
Nelor <sionjaro@5fold.de>
2016-05-26 15:04:43 +0200

    Stable Userlist implementation. Reacts to "users created" events. Important fix: the socket.on-call is executed inside the angular Zone now, which makes changes to the array update the screen properly.

--- public/app/components/userlist.component.ts
+++ public/app/components/userlist.component.ts
@@ -1,4 +1,4 @@
-import { Component, provide } from '@angular/core';
+import { Component, provide, NgZone,ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
 /*import 'zone.js';
 import 'reflect-metadata';
 */
@@ -11,33 +11,50 @@ selector:'hp-userlist',
 	providers: [ provide(userService, { useClass: userService})
 	,provide(socketService, { useClass: socketService})
 	],
-	templateUrl: 'app/templates/userlist.template.html'
+	templateUrl: 'app/templates/userlist.template.html'//,
+	//changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserlistComponent
 {
 	private userService:userService;
-	//private socket:any;
-	private users:any[] =
+	private socket:any;
+	public users:any[] =
 	[ 	{ "login":"test" },
 		{ "login":"test2"},
 		{ "login":"test3"} ];
+	private zone:NgZone;
 	
 	constructor(_userService:userService,_socketService: socketService)
 	{
+		this.zone = new NgZone({enableLargeStacktrace: true});
 		this.userService = _userService;
-		//this.socket  = _socketService.getIO();
+		this.socket  = _socketService.getIO();
+		/*setInterval(() =>
+		{
+			this._cd.markForCheck();
+		},500);*/
 	/*}
 	
 	ngOnInit()
-	{//
+	{*/
 		this.socket.on('users created', (user) =>
 		{
-			this.users.push(user);
-			console.log('Event: User <'+user.login+'> created.');
+			
+			this.zone.run(() =>
+			{
+				//this.users.push({"login":user.login});
+				this.users = [...this.users, {"login":user.login}];
+				console.log('Event: User <'+user.login+'> created.');
+				console.log(user);
+				console.log(this);
+				console.log(this.users);
+				console.log(this.users[3].login);
+				//this._cd.markForCheck();
+			});
 		});
 		console.log('testhp: Userlist registered users created.')
 		
-		this.userService.find({})
+		/*this.userService.find({})
 		.then((_users) => { this.users = _users; },
 		(err) => { console.log("hp->userlist->\n" + err); })
 	*/}
